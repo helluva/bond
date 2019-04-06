8e5252b991489ffc4b4538835d5c84c8e1e08e88
Cal Stephens <cal@calstephens.tech>
2019-04-06 17:04:42 -0400

    Cut the firebase bit

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 
-// Initialize Firebase
-var config = {
-apiKey: "AIzaSyBV8cecqa1eIJhiFP5kBYcjkWreNWO5QfI",
-authDomain: "eventexplorer-3ecd6.firebaseapp.com",
-databaseURL: "https://eventexplorer-3ecd6.firebaseio.com",
-projectId: "eventexplorer-3ecd6",
-storageBucket: "eventexplorer-3ecd6.appspot.com",
-messagingSenderId: "1018227441892"
-};
-firebase.initializeApp(config);
 
 var people = [
     {
